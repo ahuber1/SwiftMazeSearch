93a36c79ea9176a6d7b07fed5f43ad2fda1e7d98
Andrew Huber <ahuber1@umbc.edu>
2017-01-01 00:28:36 -0500

    Search algorithm works!

---
+++ DataStructures/DataStructures.m
@@ -0,0 +1,13 @@
+//
+//  DataStructures.m
+//  DataStructures
+//
+//  Created by Andrew Huber on 12/31/16.
+//  Copyright © 2016 Andrew Huber. All rights reserved.
+//
+
+#import "DataStructures.h"
+
+@implementation DataStructures
+
+@end
